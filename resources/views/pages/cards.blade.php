c30dc5dea2173e66c9e415e1c6cad8ca558fa366
RominaMalimban <romina.lyn@hotmail.it>
2023-01-30 22:13:06 +0100

    realizzazione sezione home

--- resources/views/pages/cards.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.main-layout')
-
-@section('content')
-    <h1 class="red">COMICS</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('pages.cards');
+    return view('pages.home');
 });
 
-Route::get('/card', function () {
-    return view('pages.card');
+Route::get('/details', function () {
+    return view('pages.details');
 });
