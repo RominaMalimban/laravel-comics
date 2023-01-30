bfe27a2593b6340a11fe0da77fa711e0d818c29f
RominaMalimban <romina.lyn@hotmail.it>
2023-01-30 19:06:46 +0100

    realizzazione sezione merchandise

---
+++ resources/views/components/bluesection.blade.php
@@ -0,0 +1,28 @@
+<section class="merchandise">
+    <ul class="container">
+        <li>
+            <img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}">
+            <a href="#">DIGITAL COMICS</a>
+        </li>
+
+        <li>
+            <img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}">
+            <a href="#">DC MERCHANDISE</a>
+        </li>
+
+        <li>
+            <img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}">
+            <a href="#">SUBSCRIPTION</a>
+        </li>
+
+        <li>
+            <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}">
+            <a href="#">COMIC SHOP LOCATOR</a>
+        </li>
+
+        <li>
+            <img src="{{Vite::asset('resources/img/buy-dc-power-visa.SVG')}}">
+            <a href="#">DC POWER VISA</a>
+        </li>
+    </ul>
+</section>
